81b81273ce153833e220ba26aa8a6299b1d88836
Douglas-Augusto <douglas.full.ti@gmail.com>
2023-09-02 21:28:45 -0300

    02/09/2023-21:28

---
+++ ContaBanco/src/ContarTerminal.java
@@ -0,0 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContarTerminal {
+    public static void main(String[] args) {
+
+        double saldo = 800.87;
+
+        try (Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+            System.out.println("Por favor, digite o número da Conta ");
+            int numero = scanner.nextInt();
+
+            System.out.println("Por favor, digite o número da Agência ");
+            String agencia = scanner.next();
+
+            System.out.println("Por favor, digite o seu Nome ");
+            String nome = scanner.next();
+
+            System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia
+                    + ", conta " +
+                    numero + " e seu saldo " + saldo + " já está disponível para saque.");
+        }
+
+    }
+}
